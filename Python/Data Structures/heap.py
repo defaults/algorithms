72ec3db7a47897f7d7bdaded505831f069d89d1c
Vikash Kumar <mailkumarvikash@gmail.com>
2017-08-29 22:02:32 +0530

    heap data structure

--- Python/Data Structures/heap.py
+++ Python/Data Structures/heap.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
 # Implementation of min heap
-clas MinHeap:
+class MinHeap:
 	def __init__(self):
 		self.heap = []
 		if not self.size:
 			self.size = len(heap)
-		
+
 	# get index of node's parent
 	def parent(node):
 		return (i - 1) / 2
-	
-	# get index of left child		
+
+	# get index of left child
 	def left_child(node):
 		return 2 * i + 1
-		
+
 	# get index of right child
 	def right_child(node):
 		return 2 * node + 2
@@ -28,26 +28,27 @@ clas MinHeap:
 		root = self.heap[0]
 		self.heap[0], self.heap[size - 1] = self.heap[size - 1], self.heap[0]
 		size -= 1
-		self.min_heapify(0)	
-		
+		self.min_heapify()
+
 		return root
-		
+
 	# min heapify
 	def min_heapify(self, i):
 		smallest = i
 		left = left_child(i)
 		right = right_child(i)
-		
-		if  left < size and self.heap[smallest] > heap[left]:
+
+		if  left < self.size and self.heap[smallest] > heap[left]:
 			smallest = heap[left]
-			
-		if right < size and self.heap[smallest] > heap[right]:
+
+		if right < self.size and self.heap[smallest] > heap[right]:
 			smallest = heap[right]
-			
+
 		if smallest is not i:
 			self.heap[smallest], self.heap[i] = self.heap[i], self.heap[smallest]
 			# calling min heapify method recursively
 			min_heapify(smallest)
-			
-			
+
+
 # max heap is same logic as above, just have methods like max_heapify and extract_max which are logiacally same but doing opposite thing
+
